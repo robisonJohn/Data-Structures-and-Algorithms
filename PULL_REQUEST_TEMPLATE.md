f35d260dd29e12b6c86ffc98adccd475b9c3ca19
johnrobison <robisonj8680@gmail.com>
2021-05-25 19:30:07 -0400

    Completed HW-2

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,9 +1,9 @@
 # Homework Submission Template
 | **Self Assessment**       |     |
 | :------------------------ | :-- |
-| Comfort level             | ?/5 |
+| Comfort level             | 5/5 |
 | HW Complete?              | Y   |
-| Challenge Bonus Complete? | P   |
+| Challenge Bonus Complete? | Y  |
 | CSS Bonus Complete?       | N/A |
 
 _Y = Yes, N = No, P = Partial, N/A = Not Applicable_
@@ -11,15 +11,19 @@ _Y = Yes, N = No, P = Partial, N/A = Not Applicable_
 ## Wins
 
 Did you solve this on your own, without Google's help? Describe the wins you had during tonight's homework assignment.
+Yes, this was all knowledge I already have.
 
 ## Struggles
 
 Were you stuck on a question/concept? Describe any difficulties that you had with this assignment.
+No, everything made sense.
 
 ## Questions
 
 If you have any questions about this assignment or for the instructors, please add them here!
+No, thanks!
 
 ## Comments
 
 If you have any comments/feedback you want to share or express about the assignment, here's the space where you can.
+Thank you!
--- index.js
+++ index.js
@@ -10,11 +10,11 @@
 // using console.log.
 // EXAMPLE - Try running node index.js before working on any of the parts and see what happens in the terminal.
 
-const givenVariable = 20;
-const doubledVariable = givenVariable * 2;
-const tripledVariable = givenVariable * 3;
-console.log(doubledVariable);
-console.log("EXAMPLE", "tripledVariable", tripledVariable);
+// const givenVariable = 20;
+// const doubledVariable = givenVariable * 2;
+// const tripledVariable = givenVariable * 3;
+// console.log(doubledVariable);
+// console.log("EXAMPLE", "tripledVariable", tripledVariable);
 // After running this and feeling comfortable, feel free to comment the above block of code out
 // (either give each line a // or highlight the block of code and press COMMAND - / )
 
@@ -26,9 +26,14 @@ console.log("EXAMPLE", "tripledVariable", tripledVariable);
 
 // 1. Set a variable called favPizzaTopping to equal to the name of your favorite pizza topping
 // Log this value to the console
+const favPizzaTopping = 'pepperoni';
+console.log(favPizzaTopping);
 
 // 2. Set a variable called favPizzaToppingUppercase that converts favPizzaTopping to an all uppercase string
 // Log this value to the console
+const favPizzaToppingUppercase = favPizzaTopping.toUpperCase();
+console.log(favPizzaToppingUppercase);
+
 
 const pizzaPrice = 10;
 const toppingPrice = 0.5;
@@ -36,6 +41,8 @@ const toppingPrice = 0.5;
 // 3. Write an expression that adds the pizzaPrice and the toppingPrice
 // and sets the value equal to a variable called totalPrice
 // Log the value of totalPrice to the console
+const totalPrice = pizzaPrice + toppingPrice;
+console.log(totalPrice);
 
 /********************************
  *
@@ -46,41 +53,65 @@ const toppingPrice = 0.5;
 // 4. Create a variable pizzaToppings that is equal to an array containing the following three strings:
 // 'Pepperoni', 'Mushrooms', 'Red Peppers'
 // Log pizzaToppings to the console
+const pizzaTopping = ['Pepperoni', 'Mushrooms', 'Red Peppers'];
+console.log(pizzaTopping);
 
 // 5. Oops our customer wants one more topping! Add 'Pineapple' to the end of the array
 // Log pizzaToppings to the console
+pizzaTopping.push('Pineapple');
+console.log(pizzaTopping);
 
 // 6. False Alarm! Our customer realized that Pineapple doesn't belong on pizza (at least not this one!)
 // Remove 'Pineapple' from the end of the pizzaTopping array
 // Log pizzaToppings to the console
+pizzaTopping.pop();
+console.log(pizzaTopping);
 
 // 7. Ok they finally made up their mind and they want one more topping. Add 'Onions' to the end of the array
 // Log pizzaToppings to the console
+pizzaTopping.push('Onions');
+console.log(pizzaTopping);
 
 // 8. Ok! Time to ring the customer up! Create a variable called totalToppings that is equal to the number
 // of toppings the customer wants on their pizza by checking the item count of pizzaToppings
+let totalToppings = pizzaTopping.length;
+console.log(totalToppings);
 
 // 9. Another Change!! (Can't this customer make up their mind??)
 // They want to add Basil to the pizza and they want A LOT.
 // Add 'Basil' to the beginning of the array to show it's importance
 // Log pizzaToppings to the console
+pizzaTopping.unshift('Basil');
+console.log(pizzaTopping);
+
 
 // 10. Ok! We have to ring the customer up again! reassign the variable that is equal to the number
 // of toppings the customer wants on their pizza by checking the current item count of pizzaToppings
 // (Don't create a new variable!!!!!!)
 // Log the variable to the console
+console.log(pizzaTopping.length);
 
 // 11. Your boss comes over and reminds you that you need to have a copy of the receipt for each order
 // Create a variable called receipt that is a copy of the pizzaToppings array and log the result to the console
+const pizzaToppingsReceipt = pizzaTopping.slice();
+console.log(pizzaToppingsReceipt);
 
 // 12. Your boss wants some information on your last customer. Check your receipt variable to see if the order
 // contains your boss's favorite topping - 'Basil'
+for (let i = 0; i < pizzaTopping.length; i++) {
+    if ('Basil' == pizzaTopping[i]) {
+        console.log('Yes, Basil is one of the toppings');
+    }
+}
+
 
 // 13. Now that the boss has verified Basil is on the list, he notices
 // over your shoulder that its the first item. He wants you to move the
 // list order around so that Basil is at the end because "You save the best for
 // last! Scramble it, reverse it, do whatever you have to do"
 // Find the right method to accomplish this task.
+pizzaTopping.reverse();
+console.log(pizzaTopping);
 
 /********************************
  *
@@ -91,9 +122,18 @@ const toppingPrice = 0.5;
 // Our pizza place wants to show each customer a print out of their toppings to double check
 // that there are no mistakes. Use a for loop and log 'You ordered *TOPPING* on your pizza'
 // for each item in the pizzaTopping array.
+for (let i = 0; i < pizzaTopping.length; i++) {
+    console.log(`You ordered ${pizzaTopping[i]} on your pizza`);
+}
 
 // We need to give the customer a final price. Our pizza place has decided to charge .50 cents per topping.
 // Loop over the toppings array and add .50 to the price variable for each topping.
 // Log out the price variable to the console after the loop
 
 let price = 10;
+
+for (let i = 0; i < pizzaTopping.length; i++) {
+    price = price + 0.5;
+}
+console.log(`The final price is $${price}`);
+
